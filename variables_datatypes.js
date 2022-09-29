9b3ce09fe51e50de49d4bb0d3a8d17d3bbdbc97a
Allison Valdez <ajsnipes@usfca.edu>
2022-09-29 11:54:36 -0400

    still not running

--- variables_datatypes.js
+++ variables_datatypes.js
@@ -15,3 +15,14 @@ console.log(`${dayOfBirth}`);
 let userInput = prompt("What is your favorite color?");
 
 console.log(`Thank you ${name} for your favoritite color. You know ${userInput} is such a pretty color!`)
+
+let someString = "\"double quotes\" and 'single quotes'";
+
+//Undefined means that a variable or object does not have a value. Null means a value has not been initialized to a variable or object but that it has a placeholder in the meantime as null until it is reassigned 
+
+//NaN means not a number. TypeOf NaN returns an answer that states it is not a number but a value was received
+
+let thing;
+
+console.log(typeof thing);
+
